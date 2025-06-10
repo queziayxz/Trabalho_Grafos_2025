43a062668522ce820e17dd66d33818dcbcc969e3
queziayxz <quezia.emanuelly2003@gmail.com>
2025-06-10 11:38:33 -0300

    apagando o executável

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,13 @@
 using namespace std;
 int main(int argc, char *argv[])
 {
-
-    Grafo* grafo = new Grafo();
-
-    Gerenciador::comandos(grafo);
-
+    if(argc >= 2) {
+        Grafo* grafo = new Grafo();
+    
+        Gerenciador::comandos(grafo);
+    } else {
+        cout << "por favor, informe o nome do arquivo com os dados do grafo" << endl;
+    }
+    
     return 0;
 }
