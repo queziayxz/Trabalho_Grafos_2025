dc88bfbf27b6b71984b3ed44bbf394cde171837e
emmanueln4ssif <emmanuel.nassif@gmail.com>
2025-07-31 11:35:31 -0300

    adicionando arquivo guloso

---
+++ src/Guloso.cpp
@@ -0,0 +1,19 @@
+//Emerson Caneschi Coelho de Souza
+//Emmanuel Gomes Nassif
+//Quezia Emanuelly da Silva Oliveira
+
+#include <fstream>
+#include <sstream>
+#include <cstring>
+#include <regex>
+#include "Grafo.h"
+#include <climits>
+#include "Guloso.h"
+
+Guloso::Guloso()
+{
+}
+
+Guloso::~Guloso()
+{
+}
---
+++ src/Guloso.h
@@ -0,0 +1,26 @@
+//Emerson Caneschi Coelho de Souza
+//Emmanuel Gomes Nassif
+//Quezia Emanuelly da Silva Oliveira
+
+#ifndef GULOSO_H
+#define GULOSO_H
+
+#include "No.h"
+#include "Grafo.h"
+#include "Aresta.h"
+
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+class Guloso {
+    private:
+
+    public:
+        Guloso();
+        ~Guloso();
+};
+
+#endif //GULOSO_H
